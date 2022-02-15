a3a69d59fb460a7a6bbdf8d640aab9916aef2320
Yifan Sun <sunyifan112358@gmail.com>
2022-02-15 12:12:30 -0500

    A script for baseline

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/sarchlab/akkalab
 require (
 	github.com/tebeka/atexit v0.3.0
 	gitlab.com/akita/akita/v2 v2.0.2
-	gitlab.com/akita/mem/v2 v2.0.0
-	gitlab.com/akita/mgpusim/v2 v2.0.0
+	gitlab.com/akita/mem/v2 v2.4.0
+	gitlab.com/akita/mgpusim/v2 v2.0.1
 	gitlab.com/akita/noc/v2 v2.0.2
-	gitlab.com/akita/util/v2 v2.0.0
+	gitlab.com/akita/util/v2 v2.1.0
 )
 
 //replace "gitlab.com/akita/mgpusim/v2/samples/runner" => ../mgpu_config
---
+++ run_all.py
@@ -0,0 +1,67 @@
+import subprocess
+import os
+from multiprocessing.pool import ThreadPool
+
+from sympy import EX
+
+exps = [
+    ("model1", "aes"),
+    ("model1", "atax"),
+    ("model1", "bicg"),
+    ("model1", "bitonicsort"),
+    ("model1", "conv2d"),
+    ("model1", "fastwalshtransform"),
+    ("model1", "fir"),
+    ("model1", "fft"),
+    ("model1", "floydwarshall"),
+    ("model1", "im2col"),
+    ("model1", "kmeans"),
+    ("model1", "matrixmultiplication"),
+    ("model1", "matrixtranspose"),
+    ("model1", "nbody"),
+    ("model1", "nw"),
+    ("model1", "pagerank"),
+    ("model1", "relu"),
+    ("model1", "simpleconvolution"),
+    ("model1", "spmv"),
+    ("model1", "stencil2d"),
+]
+
+
+def run_exp(exp):
+    cwd = os.getcwd()
+    metic_file_name = f'{exp[0]}_{exp[1]}_metrics'
+    cmd = f'{cwd}/{exp[0]}/{exp[0]} -timing -magic-mem-copy -benchmark={exp[1]} -metric-file-name={metic_file_name}'
+    print(cmd)
+
+    out_file_name = f'{cwd}/{exp[0]}_{exp[1]}_out.stdout'
+
+    out_file = open(out_file_name, "w")
+    out_file.write(f'Executing {cmd}\n')
+    out_file.flush()
+
+    process = subprocess.Popen(
+        cmd, shell=True, stdout=out_file, stderr=out_file, cwd=cwd)
+    process.wait()
+
+    if process.returncode != 0:
+        print("Error executing ", cmd)
+    else:
+        print("Executed ", cmd)
+
+    out_file.close()
+
+
+def main():
+    subprocess.run(["go", "build", "./..."])
+
+    tp = ThreadPool()
+    for exp in exps:
+        tp.apply_async(run_exp, args=(exp, ))
+
+    tp.close()
+    tp.join()
+
+
+if __name__ == "__main__":
+    main()
