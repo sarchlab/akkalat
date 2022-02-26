66f6fcf7ddf46d2e8915639d2021ea3c6d7ef4aa
Yifan Sun <sunyifan112358@gmail.com>
2022-02-26 10:08:44 -0500

    Add model3

---
+++ model3/runner/platform.go
@@ -0,0 +1,41 @@
+package runner
+
+import (
+	"gitlab.com/akita/akita/v2/sim"
+	"gitlab.com/akita/mgpusim/v2/driver"
+	"gitlab.com/akita/mgpusim/v2/timing/cp"
+	"gitlab.com/akita/mgpusim/v2/timing/pagemigrationcontroller"
+	"gitlab.com/akita/mgpusim/v2/timing/rdma"
+	"gitlab.com/akita/util/v2/tracing"
+)
+
+// TraceableComponent is a component that can accept traces
+type TraceableComponent interface {
+	sim.Component
+	tracing.NamedHookable
+}
+
+// A Platform is a collection of the hardware under simulation.
+type Platform struct {
+	Engine sim.Engine
+	Driver *driver.Driver
+	GPUs   []*GPU
+}
+
+// A GPU is a collection of GPU internal Components
+type GPU struct {
+	Domain           *sim.Domain
+	CommandProcessor *cp.CommandProcessor
+	RDMAEngine       *rdma.Engine
+	PMC              *pagemigrationcontroller.PageMigrationController
+	CUs              []TraceableComponent
+	L1VCaches        []TraceableComponent
+	L1SCaches        []TraceableComponent
+	L1ICaches        []TraceableComponent
+	L2Caches         []TraceableComponent
+	L1VTLBs          []TraceableComponent
+	L1STLBs          []TraceableComponent
+	L1ITLBs          []TraceableComponent
+	L2TLBs           []TraceableComponent
+	MemControllers   []TraceableComponent
+}
